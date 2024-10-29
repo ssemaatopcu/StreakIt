deeed0a6096e5f6cf345b272d4b980c35baade91
Sema Topcu <ssemaatopcu.@gmail.com>
2024-10-29 15:47:45 +0300

    Initial Commit

---
+++ StreakIt/StreakItApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakItApp.swift
+//  StreakIt
+//
+//  Created by Sema Topcu on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct StreakItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
